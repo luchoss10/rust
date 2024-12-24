fe1e419673dd6c6feb300817a9d65b9953c9cf01
Lucho <lucncd@gmail.com>
2024-12-23 22:48:24 -0500

    feat: slice type tutorial

--- slice_type/src/main.rs
+++ slice_type/src/main.rs
@@ -1,12 +1,48 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s);
+
+    s.clear(); // error!
+
+    println!("the first word is: {}", word);
+
+    let s = String::from("hello world");
+
+    let _hello = &s[0..5];
+    let _world = &s[6..11];
+
+    let s = String::from("hello");
+
+    let _slice = &s[0..2];
+    let _slice = &s[..2];
+
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let _slice = &s[3..len];
+    let _slice = &s[3..];
+
+    let s = String::from("hello");
+
+    let len = s.len();
+
+    let _slice = &s[0..len];
+    let _slice = &s[..];
+
+    let s = String::from("hello world");
+
+    let word = first_word_v2(&s);
+
+    println!("the first word is: {}", word);
 }
 
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
-        if item == b' '{
+        if item == b' ' {
             return i;
         }
     }
@@ -14,3 +50,14 @@ fn first_word(s: &String) -> usize {
     s.len()
 }
 
+fn first_word_v2(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
