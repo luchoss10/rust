7c24f0515cb81504f720b971106dbba896abe66d
Lucho <lucncd@gmail.com>
2025-01-02 23:00:43 -0500

    feat: first method example added.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -2,18 +2,54 @@ fn _area(width: u32, height: u32) -> u32 {
     width * height
 }
 
-fn area2(dimensions: (u32, u32)) -> u32 {
+fn _area2(dimensions: (u32, u32)) -> u32 {
     dimensions.0 * dimensions.1
+}
+
+fn area3(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
 
 fn main() {
     //let width1 = 30;
     //let height1 = 50;
 
-    let rect1 = (30, 50);
+    //let rect1 = (30, 50);
+
+    let rect2 = Rectangle {
+        width: 30,
+        height: 50,
+    };
 
     println!(
-        "The area of the rectangolle is {} square pixels.",
-        area2(rect1)
+        "The area of the rectangle is {} square pixels.",
+        area3(&rect2)
+    );
+
+    println!("rect2 is {:?}", rect2);
+
+    let scale = 2;
+    let rect3 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect3);
+
+    println!(
+        "The area of the rectangle is {} square pixels. (method)",
+        rect3.area()
     );
 }
